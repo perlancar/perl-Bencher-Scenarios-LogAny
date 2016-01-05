0252bfd3519438a3245bfc7d87721970f9d2c5e6
perlancar (@pc-office) <perlancar@gmail.com>
2016-01-05 21:59:33 +0700

    Release v0.02

--- lib/Bencher/Scenario/LogAny.pm
+++ lib/Bencher/Scenario/LogAny.pm
@@ -10,6 +10,13 @@ use warnings;
 our $scenario = {
     summary => 'Benchmark Log::Any',
 
+    description => <<'_',
+
+Early release. Todos include: benchmark enabled level, adapters, string
+formatting.
+
+_
+
     participants => [
         {name=>'log_trace', code_template=>'state $log = do { require Log::Any; Log::Any->get_logger }; $log->trace("")'},
         {name=>'if_trace' , code_template=>'state $log = do { require Log::Any; Log::Any->get_logger }; if ($log->is_trace) {}'},
@@ -18,10 +25,3 @@ our $scenario = {
 
 1;
 # ABSTRACT:
-
-=head1 SYNOPSIS
-
-=head1 DESCRIPTION
-
-Early release. Todos include: benchmark enabled level, adapters, string
-formatting.
